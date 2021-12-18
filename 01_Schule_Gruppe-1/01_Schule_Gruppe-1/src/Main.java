cad83fc32f364edbec34210a5fc5b34e2fd6637c
Pe0nx <83284383+Pe0nx@users.noreply.github.com>
2021-12-18 18:31:48 +0100

    Arbeitsschritt 1
    - 2 Schueler und 1 Lehrer als Person erstellt
    - jeweils unterschiedliche Werte vergeben
    - Ausgabe dieser als "Ausweis"

--- 01_Schule_Gruppe-1/01_Schule_Gruppe-1/src/Main.java
+++ 01_Schule_Gruppe-1/01_Schule_Gruppe-1/src/Main.java
@@ -1,10 +1,38 @@
 public class Main {
     public static void main(String[] args) {
         // Erstellung der Objekte
+        Person oSchueler_1 = new Person();
+        Person oSchueler_2 = new Person();
+        Person oLehrer_1 = new Person();
+
+        // Daten festlegen
+        oSchueler_1.iPersonalnummer = 1;
+        oSchueler_1.strVorname = "Daniel";
+        oSchueler_1.strName = "Baum";
+        oSchueler_1.strStrasse = "Neustädter Str. 1";
+        oSchueler_1.strOrt = "Leipzig";
+        oSchueler_1.strPLZ = "04315";
+
+        oSchueler_2.iPersonalnummer = 2;
+        oSchueler_2.strVorname = "Moritz";
+        oSchueler_2.strName = "Krause";
+        oSchueler_2.strStrasse = "Neustädter Str. 1";
+        oSchueler_2.strOrt = "Leipzig";
+        oSchueler_2.strPLZ = "04315";
+
+        oLehrer_1.iPersonalnummer = 3;
+        oLehrer_1.strVorname = "Klaus";
+        oLehrer_1.strName = "Wannemaker";
+        oLehrer_1.strStrasse = "An d. Querbreite 8";
+        oLehrer_1.strOrt = "Leipzig";
+        oLehrer_1.strPLZ = "04129";
 
         // Daten erfassen
 
         // Ausweise drucken --> Ausgabe in der Konsole
+        oSchueler_1.druckeAusweis();
+        oSchueler_2.druckeAusweis();
+        oLehrer_1.druckeAusweis();
 
         // Ausweise drucken --> Ausgabe in Datei
 
--- 01_Schule_Gruppe-1/01_Schule_Gruppe-1/src/Person.java
+++ 01_Schule_Gruppe-1/01_Schule_Gruppe-1/src/Person.java
@@ -1,7 +1,35 @@
 public class Person {
     // Attribute
+    public int iPersonalnummer;
+    public String strVorname;
+    public String strName;
+    public String strStrasse;
+    public String strOrt;
+    public String strPLZ;
 
     // Konstruktor
+    public Person() {
+        // Standardwerte setzen
+        iPersonalnummer = 0;
+        strVorname = "Max";
+        strName = "Mustermann";
+        strStrasse = "Musterstraße 1";
+        strOrt = "Muserort";
+        strPLZ = "01234";
+    }
 
     // Methoden
+    public void druckeAusweis() {
+        System.out.println("\n\n--- Ausweis ---");
+        System.out.println("Personalnummer: " + iPersonalnummer);
+        System.out.println("Vorname:        " + strVorname);
+        System.out.println("Name:           " + strName);
+        System.out.println("Straße:         " + strStrasse);
+        System.out.println("Ort:            " + strOrt);
+        System.out.println("PLZ:            " + strPLZ);
+    }
+
+    public void erfasseDaten() {
+
+    }
 }
